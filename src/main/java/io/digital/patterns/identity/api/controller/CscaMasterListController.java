6f81b7b687cf040a64814c6d6bfcc825df3502a8
Amin Mohammed-Coleman <amin@digitalpatterns.io>
2020-07-20 09:01:12 +0100

    removed unused imports

--- src/main/java/io/digital/patterns/identity/api/controller/CscaMasterListController.java
+++ src/main/java/io/digital/patterns/identity/api/controller/CscaMasterListController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
